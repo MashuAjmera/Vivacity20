f0c30d5c793e2f68abba3d79a827ec6d571b1b3e
HellWolhardZeli <aditya11maharana@gmail.com>
2019-10-17 01:58:34 +0530

    over the years added scrolling bug found at last page(redo last to top algo)

--- js/theme.js
+++ js/theme.js
@@ -1,39 +1,39 @@
-function changeone() {
-    document.querySelector(".one").style.cssText = "display:flex; width:100%;";
-    document.querySelector(".five").style.cssText = "display:none";
-    document.querySelector(".two").style.cssText = "display:none";
-    document.querySelector(".three").style.cssText = "display:none";
-    document.querySelector(".four").style.cssText = "display:none";
-}
+ function changeone() {
+     document.querySelector(".one").style.cssText = "display:flex; width:100%;";
+     document.querySelector(".five").style.cssText = "display:none";
+     document.querySelector(".two").style.cssText = "display:none";
+     document.querySelector(".three").style.cssText = "display:none";
+     document.querySelector(".four").style.cssText = "display:none";
+ }
 
-function changetwo() {
-    document.querySelector(".two").style.cssText = "display:flex; width:100%; ";
-    document.querySelector(".five").style.cssText = "display:none";
-    document.querySelector(".one").style.cssText = "display:none";
-    document.querySelector(".three").style.cssText = "display:none";
-    document.querySelector(".four").style.cssText = "display:none";
-}
+ function changetwo() {
+     document.querySelector(".two").style.cssText = "display:flex; width:100%; ";
+     document.querySelector(".five").style.cssText = "display:none";
+     document.querySelector(".one").style.cssText = "display:none";
+     document.querySelector(".three").style.cssText = "display:none";
+     document.querySelector(".four").style.cssText = "display:none";
+ }
 
-function changethree() {
-    document.querySelector(".three").style.cssText = "display:flex; width:100%;";
-    document.querySelector(".five").style.cssText = "display:none";
-    document.querySelector(".two").style.cssText = "display:none";
-    document.querySelector(".one").style.cssText = "display:none";
-    document.querySelector(".four").style.cssText = "display:none";
-}
+ function changethree() {
+     document.querySelector(".three").style.cssText = "display:flex; width:100%;";
+     document.querySelector(".five").style.cssText = "display:none";
+     document.querySelector(".two").style.cssText = "display:none";
+     document.querySelector(".one").style.cssText = "display:none";
+     document.querySelector(".four").style.cssText = "display:none";
+ }
 
-function changefour() {
-    document.querySelector(".four").style.cssText = "display:flex; width:100%;";
-    document.querySelector(".five").style.cssText = "display:none";
-    document.querySelector(".two").style.cssText = "display:none";
-    document.querySelector(".three").style.cssText = "display:none";
-    document.querySelector(".one").style.cssText = "display:none";
-}
+ function changefour() {
+     document.querySelector(".four").style.cssText = "display:flex; width:100%;";
+     document.querySelector(".five").style.cssText = "display:none";
+     document.querySelector(".two").style.cssText = "display:none";
+     document.querySelector(".three").style.cssText = "display:none";
+     document.querySelector(".one").style.cssText = "display:none";
+ }
 
-function changefive() {
-    document.querySelector(".five").style.cssText = "display:flex; width:100%;";
-    document.querySelector(".one").style.cssText = "display:none";
-    document.querySelector(".two").style.cssText = "display:none";
-    document.querySelector(".three").style.cssText = "display:none";
-    document.querySelector(".four").style.cssText = "display:none";
-}
+ function changefive() {
+     document.querySelector(".five").style.cssText = "display:flex; width:100%;";
+     document.querySelector(".one").style.cssText = "display:none";
+     document.querySelector(".two").style.cssText = "display:none";
+     document.querySelector(".three").style.cssText = "display:none";
+     document.querySelector(".four").style.cssText = "display:none";
+ }
