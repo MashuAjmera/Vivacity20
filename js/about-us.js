95318dbeba17eab0a0313729a3e2dd16ee7246d2
HellWolhardZeli <aditya11maharana@gmail.com>
2019-11-05 16:56:37 +0530

    .

---
+++ js/about-us.js
@@ -0,0 +1,40 @@
+var footfall = 0;
+
+
+
+
+
+function countfootfall() {
+    if (footfall < 30) {
+        footfall += 5;
+    }
+    document.getElementById('counterfootfall').innerHTML = footfall + 'k+';
+    setTimeout(countfootfall, 100);
+}
+
+
+
+
+var colleges = 0;
+
+function countcolleges() {
+    if (colleges < 1000) {
+        colleges += 5;
+    }
+    document.getElementById('countercolleges').innerHTML = colleges + "+";
+    setTimeout(countcolleges, 3);
+}
+
+var events = 0;
+
+function countevents() {
+    if (events < 3000) {
+        events += 10;
+    }
+    document.getElementById('counterevents').innerHTML = events + "+";
+    setTimeout(countevents, 1);
+}
+
+window.addEventListener('load', countfootfall);
+window.addEventListener('load', countcolleges);
+window.addEventListener('load', countevents);
--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,28 @@ var current = 0;
 
 
 
+
+
+function hideAddressBar() {
+    if (!window.location.hash) {
+        if (document.height < window.outerHeight) {
+            document.body.style.height = (window.outerHeight + 50) + 'px';
+        }
+
+        setTimeout(function () {
+            window.scrollTo(0, 1);
+        }, 50);
+    }
+}
+
+window.addEventListener("load", function () {
+    if (!window.pageYOffset) {
+        hideAddressBar();
+    }
+});
+
+
+
 var browser = (function () {
     var test = function (regexp) {
         return regexp.test(window.navigator.userAgent);
