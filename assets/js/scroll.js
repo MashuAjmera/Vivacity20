44ec8ea8b73110b2f65d4ca84b0a902e0321ac00
HellWolhardZeli <aditya11maharana@gmail.com>
2019-11-07 14:46:00 +0530

    pwa

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,3 +1,31 @@
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Stash the event so it can be triggered later.
+    e.preventDefault();
+    deferredPrompt = e;
+    showInstallPromotion();
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    window.addEventListener('appinstalled', (evt) => {
+        console.log('a2hs installed');
+    });
+});
+
+
+
+
+
+
+
+
+
+
 var container = document.querySelectorAll(".container");
 var current = 0;
 
