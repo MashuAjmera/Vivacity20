ad90531cc9010b63c62b13e204308cfe411f2865
HellWolhardZeli <aditya11maharana@gmail.com>
2019-11-12 18:34:29 +0530

    .

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,39 +1,64 @@
- function changeone() {
-     document.querySelector(".one").style.cssText = "display:flex; width:100%;";
-     document.querySelector(".five").style.cssText = "display:none";
-     document.querySelector(".two").style.cssText = "display:none";
-     document.querySelector(".three").style.cssText = "display:none";
-     document.querySelector(".four").style.cssText = "display:none";
- }
+function changeone() {
+    document.querySelector(".one").style.cssText = "display:flex; width:100%;";
+    document.getElementById('t1').style.cssText = 'color:#fff;';
+    document.getElementById('t3').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t2').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t4').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t5').style.cssText = 'color:rgb(0, 74, 117);';
+    document.querySelector(".five").style.cssText = "display:none";
+    document.querySelector(".two").style.cssText = "display:none";
+    document.querySelector(".three").style.cssText = "display:none";
+    document.querySelector(".four").style.cssText = "display:none";
+}
 
- function changetwo() {
-     document.querySelector(".two").style.cssText = "display:flex; width:100%; ";
-     document.querySelector(".five").style.cssText = "display:none";
-     document.querySelector(".one").style.cssText = "display:none";
-     document.querySelector(".three").style.cssText = "display:none";
-     document.querySelector(".four").style.cssText = "display:none";
- }
+function changetwo() {
+    document.querySelector(".two").style.cssText = "display:flex; width:100%;";
+    document.getElementById('t2').style.cssText = 'color:#fff;';
+    document.getElementById('t1').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t3').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t4').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t5').style.cssText = 'color:rgb(0, 74, 117);';
+    document.querySelector(".five").style.cssText = "display:none";
+    document.querySelector(".one").style.cssText = "display:none";
+    document.querySelector(".three").style.cssText = "display:none";
+    document.querySelector(".four").style.cssText = "display:none";
+}
 
- function changethree() {
-     document.querySelector(".three").style.cssText = "display:flex; width:100%;";
-     document.querySelector(".five").style.cssText = "display:none";
-     document.querySelector(".two").style.cssText = "display:none";
-     document.querySelector(".one").style.cssText = "display:none";
-     document.querySelector(".four").style.cssText = "display:none";
- }
+function changethree() {
+    document.querySelector(".three").style.cssText = "display:flex; width:100%;";
+    document.getElementById('t3').style.cssText = 'color:#fff;';
+    document.getElementById('t1').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t2').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t4').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t5').style.cssText = 'color:rgb(0, 74, 117);';
+    document.querySelector(".five").style.cssText = "display:none";
+    document.querySelector(".two").style.cssText = "display:none";
+    document.querySelector(".one").style.cssText = "display:none";
+    document.querySelector(".four").style.cssText = "display:none";
+}
 
- function changefour() {
-     document.querySelector(".four").style.cssText = "display:flex; width:100%;";
-     document.querySelector(".five").style.cssText = "display:none";
-     document.querySelector(".two").style.cssText = "display:none";
-     document.querySelector(".three").style.cssText = "display:none";
-     document.querySelector(".one").style.cssText = "display:none";
- }
+function changefour() {
+    document.querySelector(".four").style.cssText = "display:flex; width:100%;";
+    document.getElementById('t4').style.cssText = 'color:#fff;';
+    document.getElementById('t1').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t2').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t3').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t5').style.cssText = 'color:rgb(0, 74, 117);';
+    document.querySelector(".five").style.cssText = "display:none";
+    document.querySelector(".two").style.cssText = "display:none";
+    document.querySelector(".three").style.cssText = "display:none";
+    document.querySelector(".one").style.cssText = "display:none";
+}
 
- function changefive() {
-     document.querySelector(".five").style.cssText = "display:flex; width:100%;";
-     document.querySelector(".one").style.cssText = "display:none";
-     document.querySelector(".two").style.cssText = "display:none";
-     document.querySelector(".three").style.cssText = "display:none";
-     document.querySelector(".four").style.cssText = "display:none";
- }
+function changefive() {
+    document.querySelector(".five").style.cssText = "display:flex; width:100%;";
+    document.getElementById('t5').style.cssText = 'color:#fff;';
+    document.getElementById('t1').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t2').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t4').style.cssText = 'color:rgb(0, 74, 117);';
+    document.getElementById('t3').style.cssText = 'color:rgb(0, 74, 117);';
+    document.querySelector(".one").style.cssText = "display:none";
+    document.querySelector(".two").style.cssText = "display:none";
+    document.querySelector(".three").style.cssText = "display:none";
+    document.querySelector(".four").style.cssText = "display:none";
+}
