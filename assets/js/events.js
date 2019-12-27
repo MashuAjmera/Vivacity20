1f795b5cdc2aff8095fc6bcac4187d8d037f9071
HellWolhardZeli <39962775+HellWolhardZeli@users.noreply.github.com>
2019-12-27 20:25:59 +0530

    events done

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,27 +1,46 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndexA = 1;
+var slideIndexB = 1;
+var slideIndexC = 1;
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+showSlidesA(slideIndexA);
+showSlidesB(slideIndexB);
+showSlidesC(slideIndexC);
+
+function plusSlidesA(n) {
+  showSlidesA((slideIndexA += n));
 }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+function currentSlideA(n) {
+  showSlidesA((slideIndexA = n));
+}
+function plusSlidesB(n) {
+  showSlidesB((slideIndexB += n));
 }
 
-function showSlides(n) {
+function currentSlideB(n) {
+  showSlidesB((slideIndexB = n));
+}
+function plusSlidesC(n) {
+  showSlidesC((slideIndexC += n));
+}
+
+function currentSlideC(n) {
+  showSlidesC((slideIndexC = n));
+}
+
+function showSlidesB(n) {
   var i;
-  var slides = document.getElementsByClassName('mySlides');
-  var contents = document.getElementsByClassName('content');
-  var contents1 = document.getElementsByClassName('content1');
-  var dots = document.getElementsByClassName('dot');
-  var dots1 = document.getElementsByClassName('dot1');
+  var slides = document.getElementsByClassName('mySlides-B');
+  var contents = document.getElementsByClassName('content-B');
+  var contents1 = document.getElementsByClassName('content1-B');
+  var dots = document.getElementsByClassName('dot-B');
+  var dots1 = document.getElementsByClassName('dot1-B');
 
   if (n > slides.length) {
-    slideIndex = 1;
+    slideIndexB = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length;
+    slideIndexB = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
@@ -38,9 +57,83 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     contents1[i].style.display = 'none';
   }
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-  dots1[slideIndex - 1].className += ' active';
-  contents[slideIndex - 1].style.display = 'block';
-  contents1[slideIndex - 1].style.display = 'block';
+  slides[slideIndexB - 1].style.display = 'block';
+  dots[slideIndexB - 1].className += ' active';
+  dots1[slideIndexB - 1].className += ' active';
+  contents[slideIndexB - 1].style.display = 'block';
+  contents1[slideIndexB - 1].style.display = 'block';
 }
+
+
+function showSlidesA(n) {
+  var i;
+  var slides = document.getElementsByClassName('mySlides-A');
+  var contents = document.getElementsByClassName('content-A');
+  var contents1 = document.getElementsByClassName('content1-A');
+  var dots = document.getElementsByClassName('dot-A');
+  var dots1 = document.getElementsByClassName('dot1-A');
+
+  if (n > slides.length) {
+    slideIndexA = 1;
+  }
+  if (n < 1) {
+    slideIndexA = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  for (i = 0; i < dots1.length; i++) {
+    dots1[i].className = dots1[i].className.replace(' active', '');
+  }
+  for (i = 0; i < slides.length; i++) {
+    contents[i].style.display = 'none';
+  }
+  for (i = 0; i < slides.length; i++) {
+    contents1[i].style.display = 'none';
+  }
+  slides[slideIndexA - 1].style.display = 'block';
+  dots[slideIndexA - 1].className += ' active';
+  dots1[slideIndexA - 1].className += ' active';
+  contents[slideIndexA - 1].style.display = 'block';
+  contents1[slideIndexA - 1].style.display = 'block';
+}
+
+function showSlidesC(n) {
+  var i;
+  var slides = document.getElementsByClassName('mySlides-C');
+  var contents = document.getElementsByClassName('content-C');
+  var contents1 = document.getElementsByClassName('content1-C');
+  var dots = document.getElementsByClassName('dot-C');
+  var dots1 = document.getElementsByClassName('dot1-C');
+
+  if (n > slides.length) {
+    slideIndexC = 1;
+  }
+  if (n < 1) {
+    slideIndexC = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  for (i = 0; i < dots1.length; i++) {
+    dots1[i].className = dots1[i].className.replace(' active', '');
+  }
+  for (i = 0; i < slides.length; i++) {
+    contents[i].style.display = 'none';
+  }
+  for (i = 0; i < slides.length; i++) {
+    contents1[i].style.display = 'none';
+  }
+  slides[slideIndexC - 1].style.display = 'block';
+  dots[slideIndexC - 1].className += ' active';
+  dots1[slideIndexC - 1].className += ' active';
+  contents[slideIndexC - 1].style.display = 'block';
+  contents1[slideIndexC - 1].style.display = 'block';
+}
+
