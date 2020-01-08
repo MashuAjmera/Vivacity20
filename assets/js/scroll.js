49ef2606de49b63e8fdc5cd3f8941d6a6babfa5b
HellWolhardZeli <39962775+HellWolhardZeli@users.noreply.github.com>
2020-01-08 18:09:28 +0530

    .

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,73 +1,72 @@
-window.addEventListener("beforeinstallprompt", e => {
+window.addEventListener ('beforeinstallprompt', e => {
   // Stash the event so it can be triggered later.
-  e.preventDefault();
+  e.preventDefault ();
   deferredPrompt = e;
-  showInstallPromotion();
-  deferredPrompt.userChoice.then(choiceResult => {
-    if (choiceResult.outcome === "accepted") {
-      console.log("User accepted the A2HS prompt");
+  showInstallPromotion ();
+  deferredPrompt.userChoice.then (choiceResult => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log ('User accepted the A2HS prompt');
     } else {
-      console.log("User dismissed the A2HS prompt");
+      console.log ('User dismissed the A2HS prompt');
     }
     deferredPrompt = null;
   });
-  window.addEventListener("appinstalled", evt => {
-    console.log("a2hs installed");
+  window.addEventListener ('appinstalled', evt => {
+    console.log ('a2hs installed');
   });
 });
 
-var container = document.querySelectorAll(".container");
+var container = document.querySelectorAll ('.container');
 var current = 0;
 
-function hideAddressBar() {
+function hideAddressBar () {
   if (!window.location.hash) {
     if (document.height < window.outerHeight) {
-      document.body.style.height = window.outerHeight + 50 + "px";
+      document.body.style.height = window.outerHeight + 50 + 'px';
     }
 
-    setTimeout(function() {
-      window.scrollTo(0, 1);
+    setTimeout (function () {
+      window.scrollTo (0, 1);
     }, 50);
   }
 }
 
-window.addEventListener("load", function() {
+window.addEventListener ('load', function () {
   if (!window.pageYOffset) {
-    hideAddressBar();
+    hideAddressBar ();
   }
 });
 
-var browser = (function() {
-  var test = function(regexp) {
-    return regexp.test(window.navigator.userAgent);
+var browser = (function () {
+  var test = function (regexp) {
+    return regexp.test (window.navigator.userAgent);
   };
   switch (true) {
-    case test(/edge/i):
-      return "edge";
-    case test(/opr/i) && (!!window.opr || !!window.opera):
-      return "opera";
-    case test(/chrome/i) && !!window.chrome:
-      return "chrome";
-    case test(/trident/i):
-      return "ie";
-    case test(/firefox/i):
-      return "firefox";
-    case test(/safari/i):
-      return "safari";
+    case test (/edge/i):
+      return 'edge';
+    case test (/opr/i) && (!!window.opr || !!window.opera):
+      return 'opera';
+    case test (/chrome/i) && !!window.chrome:
+      return 'chrome';
+    case test (/trident/i):
+      return 'ie';
+    case test (/firefox/i):
+      return 'firefox';
+    case test (/safari/i):
+      return 'safari';
     default:
-      return "other";
+      return 'other';
   }
-})();
+}) ();
 var triggerdelta = 60;
 
-if (browser == "firefox") {
+if (browser == 'firefox') {
   triggerdelta = 0.5;
 }
 
 var delayInAddRemove = 700; //do not touch
 
-
-function currentYPosition() {
+function currentYPosition () {
   // Firefox, Chrome, Opera, Safari
   if (self.pageYOffset) return self.pageYOffset;
   // Internet Explorer 6 - standards mode
@@ -80,7 +79,7 @@ function currentYPosition() {
 
 //elm is element, query is selected already
 
-function elmYPosition(elm) {
+function elmYPosition (elm) {
   var y = elm.offsetTop;
   var node = elm;
   while (node.offsetParent && node.offsetParent != document.body) {
@@ -90,22 +89,22 @@ function elmYPosition(elm) {
   return y;
 }
 
-function smoothScroll(eID) {
-  var startY = currentYPosition();
-  var stopY = elmYPosition(eID);
+function smoothScroll (eID) {
+  var startY = currentYPosition ();
+  var stopY = elmYPosition (eID);
   var distance = stopY > startY ? stopY - startY : startY - stopY;
-  if (distance < 100) {
-    scrollTo(0, stopY);
-    return;
-  }
-  var speed = Math.round(distance / 100);
+  // if (distance < 100) {
+  //   scrollTo (0, stopY);
+  //   return;
+  // }
+  var speed = Math.round (distance / 100);
   if (speed >= 20) speed = 20;
-  var step = Math.round(distance / 25);
+  var step = Math.round (distance / 25);
   var leapY = stopY > startY ? startY + step : startY - step;
   var timer = 0;
   if (stopY > startY) {
     for (var i = startY; i < stopY; i += step) {
-      setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+      setTimeout ('window.scrollTo(0, ' + leapY + ')', timer * speed);
       leapY += step;
       if (leapY > stopY) leapY = stopY;
       timer++;
@@ -114,7 +113,7 @@ function smoothScroll(eID) {
     return;
   }
   for (var i = startY; i > stopY; i -= step) {
-    setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+    setTimeout ('window.scrollTo(0, ' + leapY + ')', timer * speed);
     leapY -= step;
     if (leapY < stopY) leapY = stopY;
     timer++;
@@ -123,79 +122,78 @@ function smoothScroll(eID) {
   return false;
 }
 
-
 var enablekeyboard = true;
 
 window.onkeydown = checkKey;
 
-function checkKey(e) {
+function checkKey (e) {
   // document.body.requestFullscreen();
 
   e = e || window.event;
 
   if (enablekeyboard) {
     enablekeyboard = false;
-    setTimeout(function() {
+    setTimeout (function () {
       this.enablekeyboard = true;
     }, delayInAddRemove);
-    if (e.keyCode == "38") {
+    if (e.keyCode == '38') {
       if (current != 0) {
         current--;
-        smoothScroll(container[current]);
+        smoothScroll (container[current]);
       }
-    } else if (e.keyCode == "40") {
+    } else if (e.keyCode == '40') {
       // snapScroll();
-      if (current == container.length-1) {
+      if (current == container.length - 1) {
         current = 0;
       } else {
         current++;
       }
 
-      smoothScroll(container[current]);
+      smoothScroll (container[current]);
     }
   }
 }
 
 //not required vars
 
-function scroll(event) {
+function scroll (event) {
   // document.body.requestFullscreen();
 
   if (event.deltaY < -1 * triggerdelta) {
     // event.preventDefault();
     // snapScroll();
 
-    window.removeEventListener("wheel", scroll);
-    setTimeout(addlistener, delayInAddRemove);
+    window.removeEventListener ('wheel', scroll);
+    setTimeout (addlistener, delayInAddRemove);
 
     if (current != 0) {
       current--;
-      smoothScroll(container[current]);
+      smoothScroll (container[current]);
     }
   } else if (event.deltaY > triggerdelta) {
     // event.preventDefault();
-    window.removeEventListener("wheel", scroll);
-    setTimeout(addlistener, delayInAddRemove);
+    window.removeEventListener ('wheel', scroll);
+    setTimeout (addlistener, delayInAddRemove);
 
     // snapScroll();
-    if (current == container.length-1) {
+    if (current == container.length - 1) {
       current = 0;
     } else {
       current++;
     }
 
-    smoothScroll(container[current]);
+    smoothScroll (container[current]);
   }
 }
 
-window.addEventListener("wheel", scroll);
+window.addEventListener ('wheel', scroll);
 
-function addlistener() {
-  window.addEventListener("wheel", scroll);
+function addlistener () {
+  window.addEventListener ('wheel', scroll);
 }
 
-function touch(event) {
-  console.log(event.touches[0].screenX);
+function touch (event) {
+  console.log (event.touches[0].screenX);
   // document.body.requestFullscreen();
 }
 
@@ -206,50 +204,50 @@ var startX, startY;
 
 var startX, startY;
 
-document.addEventListener("touchstart", touchstart);
+document.addEventListener ('touchstart', touchstart);
 
-function touchstart(event) {
+function touchstart (event) {
   var touches = event.touches;
   if (touches && touches.length) {
     startX = touches[0].pageX;
     startY = touches[0].pageY;
-    document.addEventListener("touchmove", touchmove);
+    document.addEventListener ('touchmove', touchmove);
   }
 }
 
-function touchmove(event) {
+function touchmove (event) {
   var touches = event.touches;
   if (touches && touches.length) {
-    event.preventDefault();
+    event.preventDefault ();
     var deltaX = startX - touches[0].pageX;
     var deltaY = startY - touches[0].pageY;
 
     if (deltaX >= 50) {
-      var event = new Event("swipeLeft");
-      document.dispatchEvent(event);
+      var event = new Event ('swipeLeft');
+      document.dispatchEvent (event);
     }
     if (deltaX <= -50) {
-      var event = new Event("swipeRight");
-      document.dispatchEvent(event);
+      var event = new Event ('swipeRight');
+      document.dispatchEvent (event);
     }
     if (deltaY >= 50) {
-      if (current == container.length-1) {
+      if (current == container.length - 1) {
         current = 0;
       } else {
         current++;
       }
 
-      smoothScroll(container[current]);
+      smoothScroll (container[current]);
     }
     if (deltaY <= -50) {
       if (current != 0) {
         current--;
-        smoothScroll(container[current]);
+        smoothScroll (container[current]);
       }
     }
 
-    if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
-      document.removeEventListener("touchmove", touchmove);
+    if (Math.abs (deltaX) >= 50 || Math.abs (deltaY) >= 50) {
+      document.removeEventListener ('touchmove', touchmove);
     }
   }
 }
